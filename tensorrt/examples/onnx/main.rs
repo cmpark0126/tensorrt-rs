0fcb0ae5e1cfd2c192721a0c5424c420095ae236
Chunmyong Park <chunmyong.park@kaist.ac.kr>
2022-12-05 15:16:29 +0000

    WIP

--- tensorrt/examples/onnx/main.rs
+++ tensorrt/examples/onnx/main.rs
@@ -5,7 +5,7 @@ use std::path::PathBuf;
 use tensorrt_rs::builder::{Builder, NetworkBuildFlags};
 use tensorrt_rs::context::ExecuteInput;
 use tensorrt_rs::data_size::GB;
-use tensorrt_rs::dims::Dims4;
+use tensorrt_rs::dims::{Dims4, Dims3};
 use tensorrt_rs::engine::Engine;
 use tensorrt_rs::onnx::{OnnxFile, OnnxParser};
 use tensorrt_rs::runtime::{Logger, Runtime};
@@ -54,23 +54,24 @@ fn main() {
     let engine = create_engine(&logger, file, vec![1], 1 * GB);
     let context = engine.create_execution_context();
 
-    let input_image = image::open("../assets/images/meme.jpg")
-        .unwrap()
-        .crop(0, 0, 224, 224)
-        .into_rgb8();
-    eprintln!("Image dimensions: {:?}", input_image.dimensions());
+    // let input_image = image::open("../assets/images/meme.jpg")
+    //     .unwrap()
+    //     .crop(0, 0, 224, 224)
+    //     .into_rgb8();
+    // eprintln!("Image dimensions: {:?}", input_image.dimensions());
 
-    // Convert image to ndarray
-    let array: ndarray_image::NdColor = ndarray_image::NdImage(&input_image).into();
-    println!("NdArray len: {}", array.len());
-    let mut pre_processed = Array::from_iter(array.iter().map(|&x| 1.0 - (x as f32) / 255.0));
+    // // Convert image to ndarray
+    // let array: ndarray_image::NdColor = ndarray_image::NdImage(&input_image).into();
+    // println!("NdArray len: {}", array.len());
+    // let mut pre_processed = Array::from_iter(array.iter().map(|&x| 1.0 - (x as f32) / 255.0));
 
     // Run inference
+    let mut input = ndarray::Array3::<f32>::zeros((3, 224, 224));
     let mut output = ndarray::Array1::<f32>::zeros(1000);
+    println!("(pre) input: {}", input);
     println!("(pre) output: {}", output);
-    let outputs = vec![ExecuteInput::Float(&mut output)];
     context
-        .executeV2(ExecuteInput::Float(&mut pre_processed), outputs)
+        .executeV2(ExecuteInput::Float(&mut input), vec![ExecuteInput::Float(&mut output)])
         .unwrap();
     println!("(post) output: {}", output);
 }
